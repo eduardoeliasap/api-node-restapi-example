51e006f8a238c731507713d451eca0f4451b5b7a
Eduardo Elias <eduardo.eliasap@gmail.com>
2024-11-08 14:26:14 -0300

    adding migrations and env variables

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,30 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import { knex } from './database'
+import * as crypto from "crypto";
+import { env } from './env';
 
 const app = fastify()
 
 app.get('/hello', async () => {
-    const tables = await knex ('sqlite_schema').select('*')
-    return tables
+    const transaction = await knex('transactions').select('*')
+
+    return transaction
+})
+
+app.post('/hello', async () => {
+    const transaction = await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title: 'Transacao test',
+        amount: 1000
+    }).returning('*')
+
+    return transaction
 })
 
 app.listen({
-    port: 3333
+    port: env.PORT
 }).then(() => {
-    console.log('Server is runnig!!');
+    console.log('Server is runnig on port:', env.PORT);
     
 })
